d1413085f0fdee3e5ac2239903447f656ca9caf8
Geovane Biazotto <143039053+Meniii93@users.noreply.github.com>
2023-09-12 00:07:48 +0000

    Code Guess the number Compute 2.0

--- Developing_Software_Introduction/GuessTheNumber.py
+++ Developing_Software_Introduction/GuessTheNumber.py
@@ -3,14 +3,17 @@ import random
 def guess(x):
     random_number = random.randint(1, x)
     guess = 0
+    AllGuesses = []
     while guess != random_number:
         guess = int(input(f'Guess a number between 1 and {x}: '))
+        AllGuesses.append(guess)
         if guess < random_number:
             print('Sorry, guess again, Too low.')
         elif guess > random_number:
             print('Sorry, guess again, Too high.')
             
-    print(f'Wow, congrats, you have guessed the number {random_number} correctly!') 
+    print(f'Wow, congrats, you have guessed the number {random_number} correctly!')
+    print(f'Thesee are all the guesses you have tried:  {AllGuesses}')
             
-guess(50)
+guess(25)
     
